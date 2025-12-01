4aed1ea1e8393876bb86be7ade05b8c47d2b59ea
micahspencer-png <micahspencer@isu.edu>
2025-12-01 08:47:45 -0700

    Set up event handlers for the buttons. Set up the draw line and select color program logic

--- EtchOSketch/EtchOSketch.Designer.cs
+++ EtchOSketch/EtchOSketch.Designer.cs
@@ -28,18 +28,239 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            Clear = new Button();
+            ExitButton = new Button();
+            SelectColorButton = new Button();
+            WaveformButton = new Button();
+            MenuStrip = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            selectColorToolStripMenuItem = new ToolStripMenuItem();
+            drawWaveformsToolStripMenuItem = new ToolStripMenuItem();
+            clearToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            fileToolStripMenuItem1 = new ToolStripMenuItem();
+            exitToolStripMenuItem1 = new ToolStripMenuItem();
+            editToolStripMenuItem1 = new ToolStripMenuItem();
+            selectColorToolStripMenuItem1 = new ToolStripMenuItem();
+            drawWaveformsToolStripMenuItem1 = new ToolStripMenuItem();
+            clearToolStripMenuItem1 = new ToolStripMenuItem();
+            helpToolStripMenuItem1 = new ToolStripMenuItem();
+            aboutToolStripMenuItem1 = new ToolStripMenuItem();
+            MenuStrip.SuspendLayout();
+            contextMenuStrip1.SuspendLayout();
             SuspendLayout();
             // 
-            // Form1
+            // Clear
+            // 
+            Clear.Location = new Point(733, 615);
+            Clear.Name = "Clear";
+            Clear.Size = new Size(146, 61);
+            Clear.TabIndex = 2;
+            Clear.Text = "&Clear";
+            Clear.UseVisualStyleBackColor = true;
+            Clear.Click += Clear_Click;
+            // 
+            // ExitButton
+            // 
+            ExitButton.Location = new Point(885, 615);
+            ExitButton.Name = "ExitButton";
+            ExitButton.Size = new Size(146, 61);
+            ExitButton.TabIndex = 3;
+            ExitButton.Text = "E&xit";
+            ExitButton.UseVisualStyleBackColor = true;
+            ExitButton.Click += ExitButton_Click;
+            // 
+            // SelectColorButton
+            // 
+            SelectColorButton.Location = new Point(429, 615);
+            SelectColorButton.Name = "SelectColorButton";
+            SelectColorButton.Size = new Size(146, 61);
+            SelectColorButton.TabIndex = 0;
+            SelectColorButton.Text = "&Select Color";
+            SelectColorButton.UseVisualStyleBackColor = true;
+            SelectColorButton.Click += SelectColorButton_Click;
+            // 
+            // WaveformButton
+            // 
+            WaveformButton.Location = new Point(581, 615);
+            WaveformButton.Name = "WaveformButton";
+            WaveformButton.Size = new Size(146, 61);
+            WaveformButton.TabIndex = 1;
+            WaveformButton.Text = "Draw &Waveforms";
+            WaveformButton.UseVisualStyleBackColor = true;
+            WaveformButton.Click += WaveformButton_Click;
+            // 
+            // MenuStrip
+            // 
+            MenuStrip.ImageScalingSize = new Size(20, 20);
+            MenuStrip.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, editToolStripMenuItem, helpToolStripMenuItem });
+            MenuStrip.Location = new Point(0, 0);
+            MenuStrip.Name = "MenuStrip";
+            MenuStrip.Size = new Size(1043, 28);
+            MenuStrip.TabIndex = 4;
+            MenuStrip.Text = "MenuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(46, 24);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(224, 26);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += ExitButton_Click;
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { selectColorToolStripMenuItem, drawWaveformsToolStripMenuItem, clearToolStripMenuItem });
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(49, 24);
+            editToolStripMenuItem.Text = "Edit";
+            // 
+            // selectColorToolStripMenuItem
+            // 
+            selectColorToolStripMenuItem.Name = "selectColorToolStripMenuItem";
+            selectColorToolStripMenuItem.Size = new Size(205, 26);
+            selectColorToolStripMenuItem.Text = "Select Color";
+            selectColorToolStripMenuItem.Click += SelectColorButton_Click;
+            // 
+            // drawWaveformsToolStripMenuItem
+            // 
+            drawWaveformsToolStripMenuItem.Name = "drawWaveformsToolStripMenuItem";
+            drawWaveformsToolStripMenuItem.Size = new Size(205, 26);
+            drawWaveformsToolStripMenuItem.Text = "Draw Waveforms";
+            drawWaveformsToolStripMenuItem.Click += WaveformButton_Click;
+            // 
+            // clearToolStripMenuItem
+            // 
+            clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            clearToolStripMenuItem.Size = new Size(205, 26);
+            clearToolStripMenuItem.Text = "Clear";
+            clearToolStripMenuItem.Click += Clear_Click;
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { aboutToolStripMenuItem });
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(55, 24);
+            helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(133, 26);
+            aboutToolStripMenuItem.Text = "About";
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem1, editToolStripMenuItem1, helpToolStripMenuItem1 });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(111, 76);
+            // 
+            // fileToolStripMenuItem1
+            // 
+            fileToolStripMenuItem1.DropDownItems.AddRange(new ToolStripItem[] { exitToolStripMenuItem1 });
+            fileToolStripMenuItem1.Name = "fileToolStripMenuItem1";
+            fileToolStripMenuItem1.Size = new Size(110, 24);
+            fileToolStripMenuItem1.Text = "File";
+            // 
+            // exitToolStripMenuItem1
+            // 
+            exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            exitToolStripMenuItem1.Size = new Size(116, 26);
+            exitToolStripMenuItem1.Text = "Exit";
+            // 
+            // editToolStripMenuItem1
+            // 
+            editToolStripMenuItem1.DropDownItems.AddRange(new ToolStripItem[] { selectColorToolStripMenuItem1, drawWaveformsToolStripMenuItem1, clearToolStripMenuItem1 });
+            editToolStripMenuItem1.Name = "editToolStripMenuItem1";
+            editToolStripMenuItem1.Size = new Size(110, 24);
+            editToolStripMenuItem1.Text = "Edit";
+            // 
+            // selectColorToolStripMenuItem1
+            // 
+            selectColorToolStripMenuItem1.Name = "selectColorToolStripMenuItem1";
+            selectColorToolStripMenuItem1.Size = new Size(205, 26);
+            selectColorToolStripMenuItem1.Text = "Select Color";
+            // 
+            // drawWaveformsToolStripMenuItem1
+            // 
+            drawWaveformsToolStripMenuItem1.Name = "drawWaveformsToolStripMenuItem1";
+            drawWaveformsToolStripMenuItem1.Size = new Size(205, 26);
+            drawWaveformsToolStripMenuItem1.Text = "Draw Waveforms";
+            // 
+            // clearToolStripMenuItem1
+            // 
+            clearToolStripMenuItem1.Name = "clearToolStripMenuItem1";
+            clearToolStripMenuItem1.Size = new Size(205, 26);
+            clearToolStripMenuItem1.Text = "Clear";
+            // 
+            // helpToolStripMenuItem1
+            // 
+            helpToolStripMenuItem1.DropDownItems.AddRange(new ToolStripItem[] { aboutToolStripMenuItem1 });
+            helpToolStripMenuItem1.Name = "helpToolStripMenuItem1";
+            helpToolStripMenuItem1.Size = new Size(110, 24);
+            helpToolStripMenuItem1.Text = "Help";
+            // 
+            // aboutToolStripMenuItem1
+            // 
+            aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+            aboutToolStripMenuItem1.Size = new Size(133, 26);
+            aboutToolStripMenuItem1.Text = "About";
+            // 
+            // EtchOSketch
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1043, 688);
-            Name = "Form1";
-            Text = "Form1";
+            Controls.Add(WaveformButton);
+            Controls.Add(ExitButton);
+            Controls.Add(SelectColorButton);
+            Controls.Add(Clear);
+            Controls.Add(MenuStrip);
+            MainMenuStrip = MenuStrip;
+            Name = "EtchOSketch";
+            Text = "EtchOSketch";
+            MenuStrip.ResumeLayout(false);
+            MenuStrip.PerformLayout();
+            contextMenuStrip1.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button Clear;
+        private Button ExitButton;
+        private Button SelectColorButton;
+        private Button WaveformButton;
+        private MenuStrip MenuStrip;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem selectColorToolStripMenuItem;
+        private ToolStripMenuItem drawWaveformsToolStripMenuItem;
+        private ToolStripMenuItem clearToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem1;
+        private ToolStripMenuItem exitToolStripMenuItem1;
+        private ToolStripMenuItem editToolStripMenuItem1;
+        private ToolStripMenuItem selectColorToolStripMenuItem1;
+        private ToolStripMenuItem drawWaveformsToolStripMenuItem1;
+        private ToolStripMenuItem clearToolStripMenuItem1;
+        private ToolStripMenuItem helpToolStripMenuItem1;
+        private ToolStripMenuItem aboutToolStripMenuItem1;
     }
 }
--- EtchOSketch/EtchOSketch.cs
+++ EtchOSketch/EtchOSketch.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace EtchOSketch
 {
     public partial class EtchOSketch : Form
@@ -5,6 +7,74 @@ namespace EtchOSketch
         public EtchOSketch()
         {
             InitializeComponent();
+        }
+        int oldX;
+        int oldY;
+        Color penColor = Color.Black;
+        int penWidth = 1;
+        //Program Logic---------------------------------------------------------------------------------------------------------
+
+        private void SelectColor() 
+        {
+            ColorDialog colorDialog1 = new ColorDialog();
+            colorDialog1.ShowDialog();
+            this.penColor = colorDialog1.Color;
+        }
+
+        private void DrawWaveform() 
+        { 
+        
+        }
+
+        private void Sketch(int x, int y) 
+        { 
+        
+        }
+
+        private void ClearDrawing() 
+        { 
+        
+        }
+
+
+
+        //Event Handlers--------------------------------------------------------------------------------------------------------
+        private void SelectColorButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WaveformButton_Click(object sender, EventArgs e)
+        {
+            DrawWaveform();
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            ClearDrawing();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void GraphicsDesign_MouseMove(object sender, MouseEventArgs e)
+        {
+            //this.Text = $"({e.X.ToString()},{e.Y.ToString()} Button: {e.Button})";
+
+            if (e.Button == MouseButtons.Left)
+            {
+                Sketch(e.X, e.Y);
+            }
+            this.oldX = e.X;
+            this.oldY = e.Y;
+
+            if (e.Button == MouseButtons.Middle)
+            {
+                SelectColor();
+            }
+            
         }
     }
 }
